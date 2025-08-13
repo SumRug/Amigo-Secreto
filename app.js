ea3bde9a7aa7890255ce8967440fde8d4f051b7f
SumrugLap <sumrug98@gmail.com>
2025-08-13 10:48:04 -0600

    Agregar función para agregar amigos  Esta es la primer parte, en ella se añadio la siguientes funciones :  -Validar que sea un nombre valido :    1.-Debe tener minimo 3 letras    2.-No puede estar vacio    3.-Solo se usan letras sin números o caracter especiales  -Limpiar el input cada vez que ingresas un nombre  -Guardar los nombres en amigos[].

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Crear una variable para añadir los nombres de los amigos.
+// y un regex para validar que sean nombres -
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    let amigos = [];
+    const validacion = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/u;
+
+
+    // :::::::::::::
+    //   FUNCIONES   
+    // :::::::::::::
+
+    // función agregar amigo 
+function agregarAmigo(){
+    // recolecta el valor de amigo
+    let nombreAmigo = document.getElementById("amigo").value
+    // verifica si es un nombre valido con : -regex, -espcios vacios, -minimo 3 letras
+    if (!validacion.test(nombreAmigo) || nombreAmigo === "" || nombreAmigo.length < 3){
+        alert(`El nombre no puede estar vacío, debe tener al menos 3 letras y no puede contener números.`);
+        return
+    }
+    //agrega el nombre de amigos al arreglo y limpia el input 
+    amigos.push(nombreAmigo);
+    document.getElementById("amigo").value = " ";
+}
+
+
+
+
+
