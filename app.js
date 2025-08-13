5c814055e41016ce839040481b87196d1a43668a
SumrugLap <sumrug98@gmail.com>
2025-08-13 11:08:13 -0600

    Se agrego la función de Actualizar Lista de Amigos     Se cambiaron algunas cosas del anterior commit para una mejor organizacion, se dividio en funciones de botones     y funciones que realizan cosas, estas se llaman solo dentro de los botones, creando asi "validarAgregarAmigos".
        Acerca de la funcion Actualizar Lista de Amigos se implemento lo siguiente:
        - una lista que recorre amigos[] para añadir los nombres visisblemente en la pantalla
        - se borra la lista y se vuelve a crear cada vez que se agrega un nombre, esto para
        que no haya duplicados

--- app.js
+++ app.js
@@ -4,26 +4,53 @@
 // Crear una variable para añadir los nombres de los amigos.
 // y un regex para validar que sean nombres -
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-    let amigos = [];
-    const validacion = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/u;
+let amigos = [];
+const validacion = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/u;
 
 
-    // :::::::::::::
-    //   FUNCIONES   
-    // :::::::::::::
+// :::::::::::::
+//   BOTONES   
+// :::::::::::::
 
-    // función agregar amigo 
-function agregarAmigo(){
+// ::::::: función del boton agregar amigo ::::::::::::
+function agregarAmigo() {
+    // agregar funcion de validar amigos
+    validarAgregarAmigos();
+
+    //agregar funcion de actualizar lista 
+    actualizarListaAmigos();
+}
+
+
+
+
+// :::::::::::::
+//   FUNCIONES   
+// :::::::::::::
+
+// Función para habilitar la opcion de agregar amigos 
+function validarAgregarAmigos() {
     // recolecta el valor de amigo
     let nombreAmigo = document.getElementById("amigo").value
     // verifica si es un nombre valido con : -regex, -espcios vacios, -minimo 3 letras
-    if (!validacion.test(nombreAmigo) || nombreAmigo === "" || nombreAmigo.length < 3){
+    if (!validacion.test(nombreAmigo) || nombreAmigo === "" || nombreAmigo.length < 3) {
         alert(`El nombre no puede estar vacío, debe tener al menos 3 letras y no puede contener números.`);
         return
     }
     //agrega el nombre de amigos al arreglo y limpia el input 
     amigos.push(nombreAmigo);
     document.getElementById("amigo").value = " ";
+}
+
+//Funcion para actualizar lista de amigos
+function actualizarListaAmigos() {
+    let textoLista = "";
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        textoLista += "<li>" + amigos[i] + "</li>";
+    }
+    return lista.innerHTML = textoLista;
 }
 
 
