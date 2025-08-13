00567f8769461d8c235e197e6d06f765374405e1
SumrugLap <sumrug98@gmail.com>
2025-08-13 12:45:07 -0600

    Funcion Sortear amigos se Agrego la función de sortear amigos, elc ual se maneja con el arreglo amigos [] se implemento también las sig caracteristicas :  -No se puede poner un nombre duplicado  -Detecta si solo tienes un nombre ( necesitas minimo 2 )

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 // y un regex para validar que sean nombres -
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 let amigos = [];
+let lista = document.getElementById("listaAmigos")
 const validacion = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/u;
 
 
@@ -22,6 +23,27 @@ function agregarAmigo() {
 }
 
 
+// ::::::: función del boton sortear amigo ::::::::::::
+function sortearAmigo(){
+    //verifica si amigos tiene algun dato con el tamaño
+    if(amigos.length == 0 ){
+        alert(`Agregue un nombre para sortear`)
+        return;
+    }
+      if(amigos.length == 1 ){
+        alert(`Agregue más de un nombre para sortear`)
+        return;
+    }
+    //si si tiene un dato, se usa Math para generrar un nombre aleatorio
+   console.log(amigos.length) 
+    let indiceAletorio = Math.floor(Math.random()*amigos.length);
+    resultado = document.getElementById("resultado");
+    limpiar();
+    //agrega el nombre 
+    resultado.innerHTML = "Tu amigo secreto es : " +amigos[indiceAletorio];
+
+}
+
 
 
 // :::::::::::::
@@ -31,29 +53,38 @@ function agregarAmigo() {
 // Función para habilitar la opcion de agregar amigos 
 function validarAgregarAmigos() {
     // recolecta el valor de amigo
-    let nombreAmigo = document.getElementById("amigo").value
-    // verifica si es un nombre valido con : -regex, -espcios vacios, -minimo 3 letras
-    if (!validacion.test(nombreAmigo) || nombreAmigo === "" || nombreAmigo.length < 3) {
+    let nombreAmigo = document.getElementById("amigo").value.trim();
+    // verifica si es un nombre valido con : -regex, -espacios vacios, -minimo 3 letras
+    if (!validacion.test(nombreAmigo) || nombreAmigo.length < 3) {
         alert(`El nombre no puede estar vacío, debe tener al menos 3 letras y no puede contener números.`);
+        return
+    }
+
+    //verificar que los amigos ya estan en la lista
+      if (amigos.includes(nombreAmigo)) {
+        alert(`No puede repetir el mismo nombre.`);
         return
     }
     //agrega el nombre de amigos al arreglo y limpia el input 
     amigos.push(nombreAmigo);
-    document.getElementById("amigo").value = " ";
+    limpiar();
+ 
 }
 
 //Funcion para actualizar lista de amigos
 function actualizarListaAmigos() {
-    let textoLista = "";
-    let lista = document.getElementById("listaAmigos")
-    lista.innerHTML = "";
+     let textoLista = "";
     for (let i = 0; i < amigos.length; i++) {
-        textoLista += "<li>" + amigos[i] + "</li>";
+         textoLista += "<li>" + amigos[i] + "</li>";
     }
     return lista.innerHTML = textoLista;
 }
 
-
+//limpia la caja del input y de la lista
+function limpiar (){
+       lista.innerHTML= "";
+       document.getElementById("amigo").value = "";
+}
 
 
 
